985705128e521a4151d1a32c509afc85c69ee589
Sartaj Singh Wariah <swsartaj@gmail.com>
2021-01-10 00:15:29 +0530

    Initial commit

---
+++ SLAVE/this_is_slave/this_is_slave.ino
@@ -0,0 +1,53 @@
+//this is slave
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial BTSerial(10, 11);
+
+int state = 0;
+const int led = 13;
+//const int button = 2;
+//int buttonstate = 1;
+
+void setup() 
+{
+  Serial.begin(9600);
+  BTSerial.begin(38400);
+  pinMode(led, OUTPUT);
+  digitalWrite(led, HIGH);
+  //pinMode(button, INPUT);
+  //digitalWrite(button, HIGH);
+}
+void loop() 
+{
+ if(BTSerial.available() > 0)
+ { 
+    // Checks whether data is comming from the serial port
+    Serial.println("Reading...");
+    state = BTSerial.read(); // Reads the data from the serial port
+ }
+ 
+ /* // Reading the button
+ buttonstate = digitalRead(button);
+ if (buttonstate == LOW) 
+ {
+   BTSerial.write('1'); // Sends '1' to the master to turn on LED
+ }
+ else
+ {
+   BTSerial.write('0');
+ }  
+*/
+  if (state == '1') 
+ {
+  Serial.println(1);
+  digitalWrite(led, HIGH); // LED ON
+  state = 0;
+ }
+ else if (state == '0') 
+ {
+  Serial.println(0);
+  digitalWrite(led, LOW); // LED ON
+  state = 0;
+ }
+}
---
+++ Test/Test.ino
@@ -0,0 +1,35 @@
+const int buttonPin = 12;
+int buttonState = 1;
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT_PULLUP);
+  //digitalWrite(buttonPin, HIGH);
+}
+
+void loop()
+{
+/*
+ if(BTSerial.available() > 0)
+ { 
+    // Checks whether data is comming from the serial port
+    state = BTSerial.read(); // Reads the data from the serial port
+ }
+*/
+ // Controlling the LED
+ buttonState = digitalRead(buttonPin);
+ Serial.println(buttonState);
+/*
+ if (state == '1') 
+ {
+  digitalWrite(ledPin, HIGH); // LED ON
+  state = 0;
+ }
+ else if (state == '0') 
+ {
+  digitalWrite(ledPin, LOW); // LED ON
+  state = 0;
+ }
+*/
+}
